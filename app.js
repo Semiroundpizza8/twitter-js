1cdfcae18060b0280f7b8ab28b40d8d5ccd16a28
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-09-18 17:22:22 -0500

    Finish part 5

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-
-
-const express = require('express');
+const nunjucks = require("nunjucks");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
+
+app.engine("html", nunjucks.render);
+app.set("view engine", "html");
+nunjucks.configure("views", { noCache: true });
 
 app.use(morgan("dev"));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-  })
-
-app.listen(3000, function() {
-    console.log("hello!");
+app.get("/", function (req, res) {
+    res.render('index', { title: 'Hall of Fame', people: people });
 })
 
+app.listen(3000, function () {
+    console.log("hello!");
+})
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf' },
+        { name: 'Frodo' },
+        { name: 'Hermione' }
+    ]
+}
+
+
+
+const people = [{ name: "Full" }, { name: "Stacker" }, { name: "Son" }];
