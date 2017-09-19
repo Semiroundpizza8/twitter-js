81dabb3e65289ca7202c71ec339d75664479d9a1
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-09-19 09:42:11 -0500

    section 7 in-progress

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ const nunjucks = require("nunjucks");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const volleyball = require("volleyball");
+const routes = require("./routes"); 
+const path = require("path");
+
+app.use('/', routes);
+app.use(morgan("dev"));
 
 app.engine("html", nunjucks.render);
 app.set("view engine", "html");
 nunjucks.configure("views", { noCache: true });
 
-app.use(morgan("dev"));
+// app.get("/", function (req, res) {
+//     res.render('index', { title: 'Hall of Fame', people: people });
+// })
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/", function (req, res) {
-    res.render('index', { title: 'Hall of Fame', people: people });
-})
 
 app.listen(3000, function () {
     console.log("hello!");
-})
+});
+
 var locals = {
     title: 'An Example',
     people: [
